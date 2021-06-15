ab3a18a93acc5ea8b8dcc2d3b9757884c8cf4fe0
Harshavardhan Pelleti <pradeepkumarpelleti@gmail.com>
2021-06-15 14:41:11 +0530

    first

---
+++ demo1.java
@@ -0,0 +1,21 @@
+class demo1
+{
+	{
+		int x,y;
+		x=8;
+		y=4;
+		add=x+y;
+		sub=x-y;
+		mul=x*y;
+		div=x/y;
+	 {
+		public static void main(String[] args)
+	   {
+		System.out.println("addition ="+add);
+		System.out.println("substraction ="+sub);
+		System.out.println("multiplication ="+mul);
+		System.out.println("division ="+div);
+	    }
+	 }
+	}
+}
